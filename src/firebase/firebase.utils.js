90ef4f269c5913207eabe9cef8351c9734dbdc45
Josh Turner <cs.pmosh@gmail.com>
2020-01-24 16:02:37 -0600

    integrated firebase into applicaiton, created sign in page with sign in component and sign in with Goggle

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyAM8QI4FiUOGJTyZtxMQaCQ26z2WwH8OvQ",
+    authDomain: "crwn-db-12037.firebaseapp.com",
+    databaseURL: "https://crwn-db-12037.firebaseio.com",
+    projectId: "crwn-db-12037",
+    storageBucket: "crwn-db-12037.appspot.com",
+    messagingSenderId: "267683877909",
+    appId: "1:267683877909:web:c2a277206cee62c0006a1c",
+    measurementId: "G-N8DMGDC50N"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;

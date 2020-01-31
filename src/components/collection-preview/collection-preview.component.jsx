2de31eb70562670d5ae036cdefd5979956a269b7
Josh Turner <cs.pmosh@gmail.com>
2020-01-31 16:01:18 -0600

    updated shop data with new data, updated cart utils to group itn cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -8,8 +8,8 @@ const CollectionPreview = ({title, items}) => (
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {items.filter(({id,}, idx) => idx < 4)
-            .map(({id, ...itemDetails}) => (
-                <CollectionItem key={id} {...itemDetails} />                
+            .map((item) => (
+                <CollectionItem key={item.id} item={item} />                
             ))}
         </div>
     </div>
